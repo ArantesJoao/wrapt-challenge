5893cb7d523f436005aaaef7a93d5819c976c3b3
João <arantesjoaov@gmail.com>
2023-09-15 13:56:41 -0300

    fix: error while deleting from firebase

--- src/app/api/file/[fileId]/route.ts
+++ src/app/api/file/[fileId]/route.ts
@@ -41,7 +41,11 @@ export async function DELETE(
 
   // Delete from Firebase Storage
   const storageRef = ref(storage, fileToDelete.name)
-  await deleteObject(storageRef)
+  try {
+    await deleteObject(storageRef)
+  } catch (error) {
+    // File doesn't exist
+  }
 
   const file = await prisma.file.deleteMany({
     where: {
@@ -68,12 +72,13 @@ export async function PUT(req: NextRequest, { params }: { params: IParams }) {
     throw new Error("File not found!")
   }
 
+  const extension = path.extname(fileToUpdate.name)
+  const newFileNameWithExtension = `${newName}${extension}`
+
   const folderPath = "C:/temp"
   if (fs.existsSync(folderPath)) {
     const filePath = path.join(folderPath, fileToUpdate.name)
     if (fs.existsSync(filePath)) {
-      const extension = path.extname(fileToUpdate.name)
-      const newFileNameWithExtension = `${newName}${extension}`
       const newFilePath = path.join(folderPath, newFileNameWithExtension)
       fs.renameSync(filePath, newFilePath)
     }
@@ -84,7 +89,7 @@ export async function PUT(req: NextRequest, { params }: { params: IParams }) {
       id: fileId,
     },
     data: {
-      name: newName,
+      name: newFileNameWithExtension,
     },
   })
 
