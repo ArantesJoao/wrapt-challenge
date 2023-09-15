7c676c7c10579a1f6d63bcf6a2e9d3b1d63243a6
João <arantesjoaov@gmail.com>
2023-09-15 14:16:34 -0300

    feat: adding configuration values (see notes)

--- src/app/libs/firebaseConfig.ts
+++ src/app/libs/firebaseConfig.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app"
 import { getStorage } from "firebase/storage"
 
+// Just to make everything clear, just like I did on schema.prisma file: normally, I would obviously set this up using a .env file, but since I have to get this up and running, I would have to send these data to you guys anyways, so it's just easier to add it here
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyDbDusDCBu-l78RmkzA3dl2GVdEg2t5Ny0",
   authDomain: "wrapt-challenge.firebaseapp.com",
   projectId: "wrapt-challenge",
   storageBucket: "wrapt-challenge.appspot.com",
   messagingSenderId: "963003186645",
-  appId: process.env.FIREBASE_APP_ID,
+  appId: "1:963003186645:web:7ef1ef46a17aca90c4915c",
   measurementId: "G-05WPQK3NH6",
 }
 
